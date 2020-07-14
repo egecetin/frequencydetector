e32d5164b98dd0cc6bb77edecb8fb3ded921256d
egecetin <egecetin@hotmail.com.tr>
2020-07-14 18:44:33 +0300

    init functions

--- process.c
+++ process.c
@@ -9,7 +9,7 @@
 	Input;
 
 	Output;
-		retval  : 2D window vector
+		retval  : 2D window vector.
 */
 Ipp64f** init_windows(void) {
 
@@ -31,17 +31,17 @@ Ipp64f** init_windows(void) {
 	}
 	
 	// Init windows
-	if (status = ippsSet_64f(1, ones, MAX_WLEN), !status)
+	if (status = ippsSet_64f(1, ones, MAX_WLEN), status)
 		goto cleanup;	
 	for (int i = 0; i < log2(MAX_WLEN) - log2(MIN_WLEN) + 1; ++i) {
-		if (status = ippsWinHann_64f(ones, arr[i * 5], MAX_WLEN / pow(2, i)), !status)				// Hanning
+		if (status = ippsWinHann_64f(ones, arr[i * 5], MAX_WLEN / pow(2, i)), status)				// Hanning
 			goto cleanup;
-		if (status = ippsWinHamming_64f(ones, arr[i * 5 + 1], MAX_WLEN / pow(2, i)), !status)		// Hamming
+		if (status = ippsWinHamming_64f(ones, arr[i * 5 + 1], MAX_WLEN / pow(2, i)), status)		// Hamming
 			goto cleanup;
 		cblas_dcopy(MAX_WLEN / pow(2, i), ones, 1, arr[i * 5 + 2], 1);								// Rectangle
-		if (status = ippsWinBlackmanOpt_64f(ones, arr[i * 5 + 3], MAX_WLEN / pow(2, i)), !status)	// Blackman
+		if (status = ippsWinBlackmanOpt_64f(ones, arr[i * 5 + 3], MAX_WLEN / pow(2, i)), status)	// Blackman
 			goto cleanup;		
-		if (status = ippsWinBartlett_64f(ones, arr[i * 5 + 4], MAX_WLEN / pow(2, i)), !status)		// Bartlett
+		if (status = ippsWinBartlett_64f(ones, arr[i * 5 + 4], MAX_WLEN / pow(2, i)), status)		// Bartlett
 			goto cleanup;
 	}
 
@@ -50,7 +50,7 @@ cleanup:
 		for (int i = 0; i < 5 * (log2(MAX_WLEN) - log2(MIN_WLEN) + 1); ++i) {
 			ippsFree(arr[i]);
 		}
-		ippsFree(arr); arr = 0;
+		ippsFree(arr); arr = NULL;
 	}
 	ippFree(ones);
 
@@ -64,12 +64,154 @@ cleanup:
 		Multiply with 1/N at forward transform.
 
 	Input;
-
+		fft		: FFT context
+		buffer	: Buffer for calculations
+		order	: FFT length (2^order)
 	Output;
 		retval	: Error status
 */
-ERR_STATUS createfft(IppsFFTSpec_R_64f** fft, int order)
+ERR_STATUS createFFT(IppsFFTSpec_R_64f** fft, Ipp8u** buffer, int order)
 {
-	//IPP_FFT_DIV_FWD_BY_N
-	return ippStsNoErr;
+	ERR_STATUS status = ippStsNoErr;
+	int buffSize = 0, specSize = 0, specbuffSize = 0;
+	Ipp8u *arrSpec = NULL, *arrSpecBuff = NULL;
+
+	// Calculate buffer sizes
+	if (status = ippsFFTGetSize_R_64f(order, IPP_FFT_DIV_FWD_BY_N, ippAlgHintNone, &specSize, &specbuffSize, &buffSize), status)
+		return status;
+	
+	// Init memory
+	arrSpec = ippsMalloc_8u(specSize);
+	arrSpecBuff = ippsMalloc_8u(specbuffSize);
+	*buffer = ippsMalloc_8u(buffSize);
+	if (!(arrSpec && arrSpecBuff && *buffer)) {
+		ippFree(arrSpec);
+		ippFree(arrSpecBuff);
+		ippFree(*buffer); *buffer = NULL;
+		return ippStsMemAllocErr;
+	}
+	
+	// Init context
+	status = ippsFFTInit_R_64f(fft, order, IPP_FFT_DIV_FWD_BY_N, ippAlgHintNone, arrSpec, arrSpecBuff);
+	if (!status) {
+		ippFree(*buffer);
+		*buffer = NULL;
+	}
+	ippFree(arrSpec);
+	ippFree(arrSpecBuff);
+
+	return status;
 }
+
+/** ##############################################################################################################
+	Create a FIR filter structure
+
+	Input;
+		pSpec		: Filter structure
+		filterType	: Filter type
+		rFreq		: Normalized cutoff frequency (If filter type is equal to ippsBandPass or ippsBandStop it is an two element array)
+		tapslen		: Degree of filter
+		windowType	: Window type for design
+	Output;
+		retval		: Error status
+*/
+ERR_STATUS createFilter(IppsFIRSpec_64f **pSpec, IppFilterType filterType, double *rFreq, int tapsLen, IppWinType windowType)
+{
+	ERR_STATUS status = ippStsNoErr;
+	
+	int specSize, bufSize;
+	Ipp8u *pBuffer = NULL;
+	Ipp64f *taps = NULL;
+
+	// Generate buffers for library
+	if (status = ippsFIRGenGetBufferSize(tapsLen, &bufSize), status)
+		goto cleanup;
+
+	taps = ippsMalloc_64f(tapsLen);
+	pBuffer = ippsMalloc_8u(bufSize);
+	if (!(taps && pBuffer)) { // Check allocated memory
+		status = ippStsMemAllocErr;
+		goto cleanup;
+	}
+
+	// Generate filter coeff
+	switch (filterType)
+	{
+	case ippsLowPass:
+		status = ippsFIRGenLowpass_64f(*rFreq, taps, tapsLen, windowType, ippTrue, pBuffer);
+		break;
+	case ippsHighPass:
+		status = ippsFIRGenHighpass_64f(*rFreq, taps, tapsLen, windowType, ippTrue, pBuffer);
+		break;
+	case ippsBandPass:
+		status = ippsFIRGenBandpass_64f(rFreq[0], rFreq[1], taps, tapsLen, windowType, ippTrue, pBuffer);
+		break;
+	case ippsBandStop:
+		status = ippsFIRGenBandstop_64f(rFreq[0], rFreq[1], taps, tapsLen, windowType, ippTrue, pBuffer);
+		break;
+	default:
+		status = ippStsErr;
+	}	
+	if (status)
+		goto cleanup;
+
+	// Generate structure
+	if (status = ippsFIRSRGetSize(tapsLen, ipp64f, &specSize, &bufSize), status)
+		goto cleanup;
+
+	*pSpec = (IppsFIRSpec_64f*)ippsMalloc_8u(specSize);
+	pBuffer = ippsMalloc_8u(bufSize);
+	if (!(pSpec && pBuffer)) { // Check allocated memory
+		status = ippStsMemAllocErr;
+		goto cleanup;
+	}
+
+	status = ippsFIRSRInit_64f(taps, tapsLen, ippAlgDirect, *pSpec);
+
+cleanup:
+	ippFree(taps);
+	ippFree(pBuffer);
+
+	return status;
+}
+
+/** ##############################################################################################################
+	Calculate spectrogram for offline data
+
+	Input;
+
+	Output;
+
+*/
+ERR_STATUS spectrogram(Ipp64f* data, int dataLen, Ipp64f** output, Ipp64f* window, int wlen, int overlap)
+{
+	ERR_STATUS status = ippStsNoErr;
+	int shift = wlen - overlap;
+	int max_thread = omp_get_max_threads();
+	int len = floor((dataLen - wlen) / (shift)) + 1;
+	Ipp8u** buff = ippMalloc(max_thread * sizeof(Ipp8u*));
+	IppsFFTSpec_R_64f** vfft = ippMalloc(max_thread * sizeof(IppsFFTSpec_R_64f*));
+	
+	if (!(buff && vfft)) {
+		status = ippStsMemAllocErr;
+		goto cleanup;
+	}
+
+	for (int i = 0; i < max_thread; ++i) {
+		if (status = createFFT(&vfft[i], &buff[i], round(log2(wlen))), status)
+			goto cleanup;
+	}		
+	
+	#pragma omp parallel for
+	for (int i = 0; i < len; ++i) {
+		ERR_STATUS status_local = ippStsNoErr;
+		
+		int id = omp_get_thread_num();		
+
+	}
+
+cleanup:
+
+	return status;
+}
+
--- process.h
+++ process.h
@@ -3,24 +3,29 @@
 #include <math.h>
 #include <ipp.h>
 #include <mkl.h>
-
-#define MAX_WLEN	32768
-#define MIN_WLEN	   32
-
-typedef int ERR_STATUS;
+#include <omp.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 	
+	/* ##################### Definitions ##################### */
+	#define MAX_WLEN	32768
+	#define MIN_WLEN	   32
+
+	typedef int ERR_STATUS;
+	typedef enum { ippsLowPass, ippsHighPass, ippsBandPass, ippsBandStop } IppFilterType;
+	
+	/* ##################### Functions ##################### */
+	
 	// Initialization
 	Ipp64f** init_windows(void);
 	void init_globalvar();
-	ERR_STATUS createfft();
+	ERR_STATUS createFFT(IppsFFTSpec_R_64f** fft, Ipp8u** buffer, int order);
+	ERR_STATUS createFilter(IppsFIRSpec_64f** pSpec, IppFilterType filterType, double* rFreq, int tapsLen, IppWinType windowType);
 
-	// Processing	
-	ERR_STATUS calcfft();
-	ERR_STATUS HPfilter(double* data, int len, double rFreq, int tapslen, IppWinType windowtype);
+	// Processing
+	ERR_STATUS spectrogram(Ipp64f* data, int dataLen, Ipp64f** output, Ipp64f* window, int wlen, int overlap);
 	ERR_STATUS threshold();
 	ERR_STATUS constant_freq();
 
